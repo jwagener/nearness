67906b89e1700f705b604841e31e0cea5738e23a
Johannes Wagener <johannes@wagener.cc>
2012-02-22 19:41:08 -0800

    thingify map html and special case wikipedia

--- lib/thingify.rb
+++ lib/thingify.rb
@@ -5,16 +5,20 @@ module Thingify
   TRANSLATE =
     {
       "image_url"    => "thumbnail_url",
-      "preview_html" => "preview_html",
+      "preview_html" => "html",
       "name"         => "title",
     }
 
   def self.get(url)
     embedly = Embedly.get(url)
-
-    TRANSLATE.inject({}) do |memo, (key, value)|
+    thing = TRANSLATE.inject({}) do |memo, (key, value)|
       memo[key] = embedly[value]
       memo
     end
+    if url.match /wikipedia.org/
+      thing["name"] = thing["name"].gsub(" - Wikipedia, the free encyclopedia", "")
+    end
+    thing
   end
+
 end
