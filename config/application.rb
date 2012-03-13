41fb377e36de2eb75a0403025f107ca6dbcdb6c3
Johannes Wagener <johannes@wagener.cc>
2012-03-12 23:17:41 -0700

    adding bookmarklet to precompile assets

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,7 @@ module Nearness
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += ['bookmarklet.js']
   end
 end
